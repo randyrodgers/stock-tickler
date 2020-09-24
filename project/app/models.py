a375d3fa6b2fd2371df0f71e08b0b64cb029a5fb
randyrodgers <randy.e.rodgers@gmail.com>
2020-09-24 13:45:25 -0500

    models for users and stocks

--- project/app/models.py
+++ project/app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+import re
 
-# Create your models here.
+######################## Managers ########################
+
+class StockManager( models.Manager ):
+    pass
+
+class UserManager( models.Manager ):
+    def login_validator( self, form ):
+        pass
+    def register_validator( self, form ):
+        pass 
+
+######################## Models #########################
+
+class Stock( models.Model ):
+    ticker = models.CharField( max_length = 10 )
+    watch_price = models.DecimalField()
+    created_at = models.DateTimeField( auto_now_add = True )
+    updated_at = models.DateTimeField( auto_now = True )
+    # objects = StockManager()
+
+class User( models.Model ):
+    first_name = models.CharField( max_length = 100 )
+    last_name = models.CharField( max_length = 100 )
+    email = models.EmailField( max_length = 100, unique = True )
+    password = models.CharField( max_length = 100 )
+    stocks = models.ManyToMany( Stock, related_name = "watchers" )
+    created_at = models.DateTimeField( auto_now_add = True )
+    updated_at = models.DateTimeField( auto_now = True )
+    # objects = UserManager()
