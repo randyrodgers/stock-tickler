ae8335f35e884dc702e371043b101cbf6ffc1bea
Ben Tsai <benjamintsa@gmail.com>
2020-09-26 18:22:51 -0400

    bug fixes

--- project/app/models.py
+++ project/app/models.py
@@ -88,7 +88,6 @@ class Stock( models.Model ):
     ticker = models.CharField( max_length = 10 )
     current_price = models.DecimalField( max_digits = 11, decimal_places = 2 )
     watch_price = models.DecimalField( max_digits = 11, decimal_places = 2 )
-    current_price = models.DecimalField( max_digits = 11, decimal_places =2, null = True )
     created_at = models.DateTimeField( auto_now_add = True )
     updated_at = models.DateTimeField( auto_now = True )
     objects = StockManager()
