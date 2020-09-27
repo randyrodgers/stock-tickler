959d436ef350828115e6d5bfcefae3b2afb529e2
randyrodgers <randy.e.rodgers@gmail.com>
2020-09-27 17:36:31 -0500

    adding function to poll yahoo and alert users

--- project/app/views.py
+++ project/app/views.py
@@ -131,66 +131,90 @@ def delete_stock( request, stock_id ):
     return redirect( '/' )
 
 def update_stock_watch_price( request, stock_id):
-    if request.method == "POST":
-        stock = Stock.objects.get( id = stock_id)
-        old_price = stock.watch_price
-        stock.watch_price = request.POST['watch_price']
-        stock.save()
-        subject = "Change to " + stock.ticker + " Watch Price"
-        message = "You are receiving this email to confirm that you have changed your watch price for " + stock.ticker + " from $" + str(old_price) + " to $" + str(stock.watch_price) + ". You will now receive an email notification if the stock reaches your new watch price. Thanks for using Stock Tickler!"
-        receipient = str(User.objects.get(id = request.session['logged_user_id']).email)
-        send_mail(subject, message, EMAIL_HOST_USER, [receipient], fail_silently=False)
-    return redirect('/profile')
+    if 'logged_user_id' in request.session:
+        if request.method == "POST":
+            stock = Stock.objects.get( id = stock_id)
+            old_price = stock.watch_price
+            stock.watch_price = request.POST['watch_price']
+            stock.save()
+            subject = "Change to " + stock.ticker + " Watch Price"
+            message = "You are receiving this email to confirm that you have changed your watch price for " + stock.ticker + " from $" + str(old_price) + " to $" + str(stock.watch_price) + ". You will now receive an email notification if the stock reaches your new watch price. Thanks for using Stock Tickler!"
+            receipient = str(User.objects.get(id = request.session['logged_user_id']).email)
+            send_mail(subject, message, EMAIL_HOST_USER, [receipient], fail_silently=False)
+        return redirect('/profile')
+    return redirect ( '/' )
 
 def find_stock( request ):
-    if request.method == 'POST':
-        errors = Stock.objects.find_stock_validator( request.POST )
-        if len( errors ) > 0:
-            for value in errors.values():
-                messages.error( request, value )
-            return redirect( '/add_stock' )
+    if 'logged_user_id' in request.session:
+        if request.method == 'POST':
+            errors = Stock.objects.find_stock_validator( request.POST )
+            if len( errors ) > 0:
+                for value in errors.values():
+                    messages.error( request, value )
+                return redirect( '/add_stock' )
 
-        stock_data = get_data(request.POST['ticker'], START_DATE, END_DATE)
-        cleaned_data = clean_data(stock_data, ADJ_CLOSE, START_DATE, END_DATE)
-        stock_stats = get_stats(cleaned_data)
-        current_stock_price = stock_stats['last_price']
-        
-        # Prevent duplicate listings in the Watch List
-        user = User.objects.get(id = request.session['logged_user_id'])
-        user_ticker_list = []
-        for stock in user.stocks.all():
-            user_ticker_list.append( stock.ticker )
-        if request.POST['ticker'].strip() not in user_ticker_list:
-            new_stock = Stock.objects.create(ticker = request.POST['ticker'].strip(), current_price = current_stock_price, watch_price = request.POST['watch_price'])
-            user.stocks.add(new_stock)
+            stock_data = get_data(request.POST['ticker'], START_DATE, END_DATE)
+            cleaned_data = clean_data(stock_data, ADJ_CLOSE, START_DATE, END_DATE)
+            stock_stats = get_stats(cleaned_data)
+            current_stock_price = stock_stats['last_price']
+            
+            # Prevent duplicate listings in the Watch List
+            user = User.objects.get(id = request.session['logged_user_id'])
+            user_ticker_list = []
+            for stock in user.stocks.all():
+                user_ticker_list.append( stock.ticker )
+            if request.POST['ticker'].strip() not in user_ticker_list:
+                new_stock = Stock.objects.create(ticker = request.POST['ticker'].strip(), 
+                                                current_price = current_stock_price, 
+                                                watch_price = request.POST['watch_price'])
+                user.stocks.add(new_stock)
 
-        return redirect('/profile')
+            return redirect('/profile')
+    return redirect( '/' )
 
-'''
-def alert_if_watch_price_met():
+def poll_yahoo_and_alert_if_watch_price_met():
     master_ticker_list = []
 
+    # populate the master ticker list
     for stock in Stock.objects.all():
         if stock.ticker not in master_ticker_list:
             master_ticker_list.append( stock.ticker )
 
+    # use master ticker list to ...
     for ticker in master_ticker_list:
+
+        # poll yahoo, ...
         stock_data = get_data( ticker, START_DATE, END_DATE )
         cleaned_data = clean_data( stock_data, ADJ_CLOSE, START_DATE, END_DATE )
         stock_stats = get_stats( cleaned_data )
         new_stock_price = stock_stats['last_price']
 
-
-
-
-        # grab updated data
+        # alert users by ...
         for user in User.objects.all():
-            if stock in user.stocks.all():
-                # if NP >= WP and NP > CP:
-                    # send email to user about stock
-                # elif NP <= WP and NP < CP:
-                    # send email to user about stock
-'''
+            stocks_list = user.stocks.filter( ticker = ticker )
+            if len( stocks_list ) > 0:
+                stock = user.stocks.get( ticker = ticker )
+                if new_stock_price >= stock.watch_price and new_stock_price > stock.current_price:
+                    # sending an email about the watch price being met
+                    subject = "Watch Price for " + stock.ticker + " Met"
+                    message = "You are receiving this email, because the current price for " + stock.ticker + ", $" + str( new_stock_price ) + ", " + \
+                              "has met your watch price of $" + str( stock.current_price ) + \
+                              ".\nThanks for using Stock Tickler!"
+                    receipient = str( user.email )
+                    send_mail( subject, message, EMAIL_HOST_USER, [receipient], fail_silently = False )
+                elif new_stock_price <= stock.watch_price and new_stock_price < stock.current_price:
+                    # sending an email about the watch price being met
+                    subject = "Watch Price for " + stock.ticker + " Met"
+                    message = "You are receiving this email, because the current price for " + stock.ticker + ", $" + str( new_stock_price ) + ", " + \
+                              "has met your watch price of $" + str( stock.current_price ) + \
+                              ".\nThanks for using Stock Tickler!"
+                    receipient = str( user.email )
+                    send_mail( subject, message, EMAIL_HOST_USER, [receipient], fail_silently = False )
+        
+                # and update the stock
+                stock = Stock.objects.get( ticker = ticker )
+                stock.current_price = new_stock_price
+                stock.save()
 
 #################### Helper Methods for Getting Stock ################
 def get_data (ticker, start, end):
